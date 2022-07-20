02d83a014320c13b34b5b855316aaf8fd1402f8b
unknown <tavanhuyvp8590@gmail.com>
2022-07-20 17:40:24 +0700

    BAIF THI CUOI MD1

---
+++ Category.js
@@ -0,0 +1,23 @@
+class Category {
+    id;
+    name;
+
+
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    getId (){
+        return this.id
+
+    }
+    setId(Id){
+        this.id = id;
+    }
+    getName(){
+        return this.name;
+    }
+    setName(name){
+        this.name=name;
+    }
+}
